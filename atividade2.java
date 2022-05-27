8a78ec081ccb2624ec46c04c5755ea99f4546b99
Julia <102470780+juliaJCM@users.noreply.github.com>
2022-05-27 11:12:01 -0300

    Add files via upload

---
+++ atividade2.java
@@ -0,0 +1,56 @@
+package Puc;
+
+import java.util.Scanner;
+
+public class atividade2 {
+
+	public static void proced(int [] valor) {
+		Scanner ent = new Scanner(System.in);
+		for(int i=0; i <valor.length; i++) {
+			System.out.println("Digite os valores");
+			valor [i] = ent.nextInt();
+		}
+	}
+	
+	public static int [] func(int [] valor) {
+		int quant = 0;
+		for(int i=0; i <valor.length; i++) {
+			if (valor [i] < 0) {
+				quant++;
+			}
+		}
+		
+		int [] neg = new int [quant];
+		int j = 0;
+		for(int i=0; i <valor.length; i++) {
+			if (valor [i] < 0) {
+				neg [j] = valor[i];
+				j++;
+			}
+		}
+		return neg;
+	}
+	
+	public static void exibe(int [] valor) {
+
+		for(int i=0; i <valor.length; i++) {
+			System.out.println(valor[i]);			
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int [] vet = new int [10];
+		
+		proced(vet);
+		func(vet);
+		System.out.println("Os valores negativos são:");
+		exibe(func(vet));
+		
+		
+ent.close();
+	}
+
+}
+
+
